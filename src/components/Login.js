6823e6de654d45b055776bf7341ee1be4cb7b001
Dan <b5014133@my.shu.ac.uk>
2019-03-09 19:57:10 +0000

    Created simple login page, need to add redirect if successful

---
+++ src/components/Login.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import axios from 'axios'
+import jwt_decode from 'jwt-decode';
+import setAuthToken from '../utils/setAuthToken';
+import '../styles/login.css'
+
+export default class Login extends Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			postCode: '',
+			userCode: ''
+		}
+	}
+
+	onChange = (e) => {
+		this.setState({[e.target.name]: e.target.value});
+	}
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+
+		const loginData = {
+			postCode: this.state.postCode,
+			userCode: this.state.userCode
+		}
+		axios.post('http://evoting-endpoint-evoting-endpoint.1d35.starter-us-east-1.openshiftapps.com/users/login', loginData)
+			.then((res) => {
+				const token = res.data;
+				localStorage.setItem('jwtToken', token);
+				setAuthToken(token);
+			})
+			.catch((err) => {
+				console.log(err);
+			})
+	}
+	
+	render() {
+		return (
+			<div id="login-container">
+				<Row>
+					<Col md={{ span: 8, offset: 2}}>
+						<div id="login-form">
+							<h1>Login</h1>
+							<Form onSubmit={this.handleSubmit}>
+								<Form.Group controlId="formBasicEmail">
+									<Form.Label>
+										Please enter the postcode (or your country's equivalent) you registered with:
+									</Form.Label>
+									<Form.Control type="text" placeholder="Post Code" 
+										name="postCode" onChange={this.onChange} 
+										value={this.state.postCode} />
+									<Form.Text className="text-muted">
+										We'll never share your email with anyone else.
+									</Form.Text>
+								</Form.Group>
+
+								<Form.Group controlId="formBasicPassword">
+									<Form.Label>
+										Please enter your unique voting code you received in the mail:
+									</Form.Label>
+									<Form.Control type="text" placeholder="Voting Code" 
+										name="userCode" onChange={this.onChange}
+										value={this.state.userCode} />
+								</Form.Group>
+								<Button variant="primary" type="submit">
+									Log In
+								</Button>
+							</Form>
+
+						</div>
+					</Col>
+				</Row>
+			</div>
+		)
+	}
+}
