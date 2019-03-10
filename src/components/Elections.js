bd9bd50270898074c84333d251baaa12e3ce73de
Dan <b5014133@my.shu.ac.uk>
2019-03-10 20:36:00 +0000

    Skeleton code for election page

---
+++ src/components/Elections.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Elections extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Elections list</h1>
+            </div>
+        );
+    }
+}
+
+export default Elections;
