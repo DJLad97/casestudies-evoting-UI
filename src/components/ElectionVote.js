ee7a8c4d310911b64ef4a746d09578919c2179f9
Dan <b5014133@my.shu.ac.uk>
2019-03-14 23:44:36 +0000

    Create custom radio buttons to select candidates

--- src/components/ElectionVote.js
+++ src/components/ElectionVote.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+import '../styles/election-vote.css';
 
 class ElectionVote extends Component {
     constructor(props) {
@@ -8,11 +11,50 @@ class ElectionVote extends Component {
         }
     }
     
+    renderCandidates = () => {
+        return this.state.election.candidates.map((candidate, index) => {
+            return (
+                <Col md={{ span: 8, offset: 2}} key={index}>
+                    <div className="candidate">
+                        {/* <input type="radio" className="candidate" name="vote" id={candidate.candidateName}/>
+                        <label htmlFor={candidate.candidateName}>
+                            {candidate.candidateName}<br/>
+                            {candidate.party}
+                        </label> */}
+                        <label className="radio-container">
+                            <p className="candidate-title">
+                                <strong>{candidate.candidateName}</strong>
+                                <br/>
+                                {candidate.party}
+                            </p>
+                            <input type="radio" name="vote"/>
+                            <span className="checkmark"></span>
+                        </label>
+                    </div>
+                </Col>
+            )
+        })
+    }
     render() {
         console.log(this.state.election);
         return (
             <div>
-                {/* {this.props.location.state.} */}
+                <div className="page-content-box">
+                    <h4 id="instructions">
+                        Please click on the candidate you wish to vote for
+                        <br/>
+                        You will only be able to choose one candidate
+                    </h4>
+                    {/* {this.props.location.state.} */}
+                        <form>
+                            {/* <input type="radio" className="candidate" name="radio3" id="jeremy"/><label htmlFor="jeremy">Jeremy</label> */}
+                            {/* <input type="radio" className="candidate" name="radio3" id="theresa"/><label htmlFor="theresa">Theresa</label> */}
+                            {/* <input type="radio" className="candidate" name="radio3" id="boris"/><label htmlFor="boris">Boris</label> */}
+                            <Row>
+                                {this.renderCandidates()}
+                            </Row>
+                        </form>
+                </div>
             </div>
         );
     }
