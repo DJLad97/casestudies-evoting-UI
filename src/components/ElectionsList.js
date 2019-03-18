efa8e6670f9bf4d91a3a3bce2df9cf12975919ba
NatsSsor <b5021365@my.shu.ac.uk>
2019-03-18 10:41:08 +0000

    Fixed merge issue (thanks github)
    to resolve previous issues if master was checked out

--- src/components/ElectionsList.js
+++ src/components/ElectionsList.js
@@ -1,90 +1,54 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Row, Button, Col }  from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import axios from "axios";
+import { Row, Button, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-import auth from '../utils/auth';
-import ElectionLink from './ElectionLink';
+import auth from "../utils/auth";
+import ElectionLink from "./ElectionLink";
 
-import '../styles/elections-list.css';
+import "../styles/elections-list.css";
 
 class ElectionsList extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			currentElections: [],
-			loading: true
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentElections: [],
+      loading: true
+    };
+  }
 
   componentDidMount() {
     const headers = {
       headers: {
         "x-access-token": auth.getInstance().getToken(),
-        "x-access-token2": auth.getInstance().getConsToken(),
+        "x-access-token2": auth.getInstance().getConsToken()
       }
     };
     // console.log(axios.defaults.headers);
     let baseurl = auth.getInstance().getUserInfo().expectedEndpoint;
 
-    axios
-      .get(
-        baseurl+"/elections/current",
-        headers
-      )
-      .then(res => {
-        // console.log(res);
-        this.setState({ currentElections: res.data, loading: false });
-      });
+    axios.get(baseurl + "/elections/current", headers).then(res => {
+      // console.log(res);
+      this.setState({ currentElections: res.data, loading: false });
+    });
     // console.log(auth.getInstance().getUserInfo());
   }
 
-	renderElections = () => {
-		return this.state.currentElections.map((election, index) => {
-			const formattedName = election.electionName
-			.toLowerCase()
-			.replace(/-/g, "")
-			.replace(/ /g, "-");
-			return (
-			<React.Fragment key={index}>
-				<Link
-				key={index}
-				to={{ pathname: `/election/${formattedName}`, state: { election } }}
-				>
-				<ElectionLink name={election.electionName} />
-				</Link>
+  renderElections = () => {
+    return this.state.currentElections.map((election, index) => {
+      const formattedName = election.electionName
+        .toLowerCase()
+        .replace(/-/g, "")
+        .replace(/ /g, "-");
+      return (
+        <React.Fragment key={index}>
+          <Link
+            key={index}
+            to={{ pathname: `/election/${formattedName}`, state: { election } }}
+          >
+            <ElectionLink name={election.electionName} />
+          </Link>
 
-<<<<<<< HEAD
-				<Button className="AsAuditor" variant="outline-dark">
-				<Link
-					key={index}
-					to={{ pathname: `/audit/${formattedName}`, state: { election } }}
-				>
-					{" "}
-					View as Auditor
-				</Link>
-				</Button>
-			</React.Fragment>
-			);
-		});
-	};
-
-	render() {
-	return (
-		<div className="page-content-box elections-list">
-			<h3 id="elections-list-header">
-				Please choose the election you wish to vote in
-			</h3>
-			<Row>
-				{this.renderElections()}
-				<Col md={{ span: 8, offset: 2 }}>
-					{this.state.loading && <div className="lds-dual-ring" />}
-				</Col>
-			</Row>
-		</div>
-	);
-	}
-=======
           <Button className="AsAuditor" variant="outline-dark">
             <Link
               key={index}
@@ -130,7 +94,6 @@ class ElectionsList extends Component {
       </div>
     );
   }
->>>>>>> AuditorBranch
 }
 
 export default ElectionsList;
