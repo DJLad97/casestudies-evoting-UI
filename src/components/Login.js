62b4f6af923b380a9e4f7dbc7770324ce5827ef1
tandyshane <b5020350@my.shu.ac.uk>
2019-03-18 18:18:57 +0000

    Added a name to login button for tests

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,103 +1,118 @@
-import React, { Component } from 'react'
-import PubSub from 'pubsub-js'
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import axios from 'axios'
-import RegisterModalButton from './RegisterModalButton';
+import React, { Component } from "react";
+import PubSub from "pubsub-js";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import axios from "axios";
+import RegisterModalButton from "./RegisterModalButton";
 
-import auth from '../utils/auth';
+import auth from "../utils/auth";
 
-import '../styles/login.css';
+import "../styles/login.css";
 
 export default class Login extends Component {
-	constructor(props) {
-		super(props);
-		
-		this.state = {
-			postCode: '',
-			userCode: ''
-		}
-	}
+  constructor(props) {
+    super(props);
 
-	componentDidMount(){
-		if(auth.getInstance().isAuthenticated()){
-			PubSub.publish('navigation', '/elections');
-		}
-	}
+    this.state = {
+      postCode: "",
+      userCode: ""
+    };
+  }
 
-	onChange = (e) => {
-		this.setState({[e.target.name]: e.target.value});
-	}
+  componentDidMount() {
+    if (auth.getInstance().isAuthenticated()) {
+      PubSub.publish("navigation", "/elections");
+    }
+  }
 
-	handleSubmit = (e) => {
-		e.preventDefault();
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-		const loginData = {
-			postCode: this.state.postCode.replace(/\s/g,'').toLowerCase(),
-			userCode: this.state.userCode
-		}
-		axios.post('http://evoting-endpoint-evoting-endpoint.1d35.starter-us-east-1.openshiftapps.com/users/login', loginData)
-			.then((res) => {
-				const token = res.data;
-				auth.getInstance().setToken(token);
+  handleSubmit = e => {
+    e.preventDefault();
 
-				var userInfo = auth.getInstance().getUserInfo();
+    const loginData = {
+      postCode: this.state.postCode.replace(/\s/g, "").toLowerCase(),
+      userCode: this.state.userCode
+    };
+    axios
+      .post(
+        "http://evoting-endpoint-evoting-endpoint.1d35.starter-us-east-1.openshiftapps.com/users/login",
+        loginData
+      )
+      .then(res => {
+        const token = res.data;
+        auth.getInstance().setToken(token);
 
-				axios.post(userInfo.expectedEndpoint+"/elections/login", loginData)
-				.then((newRes) => {
-					const endpointToken = newRes.data;
-					auth.getInstance().setConstiuencyToken(endpointToken);
+        var userInfo = auth.getInstance().getUserInfo();
 
-					PubSub.publish('navigation', '/elections');
-				})
-				.catch((err) => {
-					console.log(err);
-				})
+        axios
+          .post(userInfo.expectedEndpoint + "/elections/login", loginData)
+          .then(newRes => {
+            const endpointToken = newRes.data;
+            auth.getInstance().setConstiuencyToken(endpointToken);
 
-			})
-			.catch((err) => {
-				console.log(err);
-			})
-	}
-	
-	render() {
-		return (
-			<div id="login-container">
-				<Row>
-					<Col md={{ span: 8, offset: 2}}>
-						<div className="page-content-box">
-							<h1>Login</h1>
-							<Form onSubmit={this.handleSubmit}>
-								<Form.Group controlId="formBasicPostcode">
-									<Form.Label>
-										Please enter the postcode (or your country's equivalent) you registered with:
-									</Form.Label>
-									<Form.Control type="text" placeholder="Post Code" 
-										name="postCode" onChange={this.onChange} 
-										value={this.state.postCode} required/>
-								</Form.Group>
+            PubSub.publish("navigation", "/elections");
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-								<Form.Group controlId="formBasicVotingCode">
-									<Form.Label>
-										Please enter your unique voting code you received in the mail:
-									</Form.Label>
-									<Form.Control type="text" placeholder="Voting Code" 
-										name="userCode" onChange={this.onChange}
-										value={this.state.userCode} required />
-								</Form.Group>
-								<Button variant="primary" type="submit">
-									Log In
-								</Button>
-							</Form>
+  render() {
+    return (
+      <div id="login-container">
+        <Row>
+          <Col md={{ span: 8, offset: 2 }}>
+            <div className="page-content-box">
+              <h1>Login</h1>
+              <Form onSubmit={this.handleSubmit}>
+                <Form.Group controlId="formBasicPostcode">
+                  <Form.Label>
+                    Please enter the postcode (or your country's equivalent) you
+                    registered with:
+                  </Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Post Code"
+                    name="postCode"
+                    onChange={this.onChange}
+                    value={this.state.postCode}
+                    required
+                  />
+                </Form.Group>
 
-							<RegisterModalButton/>
+                <Form.Group controlId="formBasicVotingCode">
+                  <Form.Label>
+                    Please enter your unique voting code you received in the
+                    mail:
+                  </Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Voting Code"
+                    name="userCode"
+                    onChange={this.onChange}
+                    value={this.state.userCode}
+                    required
+                  />
+                </Form.Group>
+                <Button name="btnLogin" variant="primary" type="submit">
+                  Log In
+                </Button>
+              </Form>
 
-						</div>
-					</Col>
-				</Row>
-			</div>
-		)
-	}
+              <RegisterModalButton />
+            </div>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
