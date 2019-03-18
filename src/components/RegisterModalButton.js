65dd9891a2d9afccde4f7594dc37f03a7fd2e592
tandyshane <b5020350@my.shu.ac.uk>
2019-03-18 18:36:31 +0000

    Register button now has a name for testing purposes

--- src/components/RegisterModalButton.js
+++ src/components/RegisterModalButton.js
@@ -1,233 +1,249 @@
-import React, { Component } from 'react';
-import { Form, Modal } from 'react-bootstrap';
-import axios from 'axios';
-import userAPI from '../utils/userAPI';
-import CountryDropdown from './CountryDropdown';
-import NationalityDropdown from './NationalityDropdown';
+import React, { Component } from "react";
+import { Form, Modal } from "react-bootstrap";
+import axios from "axios";
+import userAPI from "../utils/userAPI";
+import CountryDropdown from "./CountryDropdown";
+import NationalityDropdown from "./NationalityDropdown";
 
-import Tab from 'react-bootstrap/Tab'
-import Row from 'react-bootstrap/Row'
-import Nav from 'react-bootstrap/Nav'
-import Col from 'react-bootstrap/Col'
+import Tab from "react-bootstrap/Tab";
+import Row from "react-bootstrap/Row";
+import Nav from "react-bootstrap/Nav";
+import Col from "react-bootstrap/Col";
 
 class RegisterModalButton extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            modalIsOpen: false,
-            firstName : "",
-            lastName : "",
-            postCode : "",
-            isAuditor : false,
-            countryId : "",
-            nationality : "",
-            dateOfBirth : "",
-            fullAddress : ""
-        };
-      
-        this.openModal = this.openModal.bind(this);
-        this.afterOpenModal = this.afterOpenModal.bind(this);
-        this.closeModal = this.closeModal.bind(this);
+    this.state = {
+      modalIsOpen: false,
+      firstName: "",
+      lastName: "",
+      postCode: "",
+      isAuditor: false,
+      countryId: "",
+      nationality: "",
+      dateOfBirth: "",
+      fullAddress: ""
+    };
 
-        this.getRegisterForm = this.getRegisterForm.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
 
-        this.submitNewUser = this.submitNewUser.bind(this);
+    this.getRegisterForm = this.getRegisterForm.bind(this);
+
+    this.submitNewUser = this.submitNewUser.bind(this);
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  submitNewUser(event) {
+    function _getAge(dateString) {
+      var today = new Date();
+      var birthDate = new Date(dateString);
+      var age = today.getFullYear() - birthDate.getFullYear();
+      var m = today.getMonth() - birthDate.getMonth();
+      if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+      return age;
     }
 
-    onChange = (e) => {
-        this.setState({[e.target.name]: e.target.value})
+    event.preventDefault();
+
+    if (this.state.firstName == "" || this.state.firstName == null) {
+      alert("First name is a required field!");
+      return;
+    }
+    if (this.state.lastName == "" || this.state.lastName == null) {
+      alert("Last name is a required field!");
+      return;
+    }
+    if (this.state.postCode == "" || this.state.postCode == null) {
+      alert("PostCode is a required field!");
+      return;
+    }
+    if (this.state.countryId == "" || this.state.countryId == null) {
+      alert("Country is a required field!");
+      return;
+    }
+    if (this.state.nationality == "" || this.state.nationality == null) {
+      alert("Nationality is a required field!");
+      return;
+    }
+    if (_getAge(this.state.dateOfBirth) < 18) {
+      alert("User must be 18 to register to vote!");
+      return;
+    }
+    if (this.state.fullAddress == "" || this.state.fullAddress == null) {
+      alert("Full Address is a required field!");
+      return;
     }
 
-    submitNewUser(event) {
-        function _getAge(dateString) 
-        {
-            var today = new Date();
-            var birthDate = new Date(dateString);
-            var age = today.getFullYear() - birthDate.getFullYear();
-            var m = today.getMonth() - birthDate.getMonth();
-            if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) 
-            {
-                age--;
-            }
-            return age;
-        }
+    let data = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      isAuditor: false,
+      postCode: this.state.postCode.replace(/\s/g, "").toLowerCase(),
+      countryId: this.state.countryId,
+      nationality: this.state.nationality,
+      dateOfBirth: this.state.dateOfBirth,
+      fullAddress: this.state.fullAddress
+    };
 
-        event.preventDefault();
+    userAPI
+      .getInstance()
+      .registerUser(data)
+      .then(res => {
+        if (res.success == true) {
+          alert(
+            `Account Created! (PROTOTYPE: YOU RECEIVED THE FOLLOWING CODE IN THE MAIL... ${res.userCode.toString()})`
+          );
+          this.closeModal();
+        } else alert("Something went wrong, please try again later");
+      });
+  }
 
-        if (this.state.firstName == "" || this.state.firstName == null)
-        {
-            alert("First name is a required field!");
-            return;
-        }
-        if (this.state.lastName == "" || this.state.lastName == null)
-        {
-            alert("Last name is a required field!");
-            return;
-        }
-        if (this.state.postCode == "" || this.state.postCode == null)
-        {
-            alert("PostCode is a required field!");
-            return;
-        }
-        if (this.state.countryId == "" || this.state.countryId == null)
-        {
-            alert("Country is a required field!");
-            return;
-        }
-        if (this.state.nationality == "" || this.state.nationality == null)
-        {
-            alert("Nationality is a required field!");
-            return;
-        }
-        if (_getAge(this.state.dateOfBirth) < 18)
-        {
-            alert("User must be 18 to register to vote!");
-            return;
-        }
-        if (this.state.fullAddress == "" || this.state.fullAddress == null)
-        {
-            alert("Full Address is a required field!");
-            return;
-        }
+  openModal() {
+    this.setState({ modalIsOpen: true });
 
-        let data = {
-            firstName: this.state.firstName,
-            lastName: this.state.lastName,
-            isAuditor: false,
-            postCode: this.state.postCode.replace(/\s/g,'').toLowerCase(),
-            countryId: this.state.countryId,
-            nationality: this.state.nationality,
-            dateOfBirth: this.state.dateOfBirth,
-            fullAddress: this.state.fullAddress,
-        };
+    this.setState({ firstName: "" });
+    this.setState({ lastName: "" });
+    this.setState({ postCode: "" });
+    this.setState({ countryId: "" });
+    this.setState({ nationality: "" });
+    this.setState({ dateOfBirth: "" });
+    this.setState({ fullAddress: "" });
+  }
 
-        userAPI.getInstance().registerUser(data)
-        .then(res => {
-            if (res.success == true) {
-                alert(`Account Created! (PROTOTYPE: YOU RECEIVED THE FOLLOWING CODE IN THE MAIL... ${res.userCode.toString()})`);
-                this.closeModal();
-            }
-            else
-                alert("Something went wrong, please try again later");
-        });
-    }
+  afterOpenModal() {
+    this.subtitle.style.color = "#f00";
+  }
 
-    openModal() {
-        this.setState({modalIsOpen: true});
+  closeModal() {
+    this.setState({ modalIsOpen: false });
+  }
 
-        this.setState({firstName: ""});
-        this.setState({lastName: ""});
-        this.setState({postCode: ""});
-        this.setState({countryId: ""});
-        this.setState({nationality: ""});
-        this.setState({dateOfBirth: ""});
-        this.setState({fullAddress: ""});
-    }
+  getRegisterForm() {
+    return (
+      <div>
+        <form onSubmit={this.submitNewUser}>
+          <p>(* indicates a required field)</p>
 
-    afterOpenModal() {
-        this.subtitle.style.color = '#f00';
-    }
+          <Form.Group>
+            <Form.Label>First Name: *</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="First Name"
+              name="firstName"
+              onChange={this.onChange}
+              value={this.state.firstName}
+              required
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Last Name: *</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Last Name"
+              name="lastName"
+              onChange={this.onChange}
+              value={this.state.lastName}
+              required
+            />
+          </Form.Group>
 
-    closeModal() {
-        this.setState({modalIsOpen: false});
-    }
+          <Form.Group>
+            <Form.Label>Postcode: *</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Postcode"
+              name="postCode"
+              onChange={this.onChange}
+              value={this.state.postCode}
+              required
+            />
+          </Form.Group>
 
-    getRegisterForm() {
-        return (
-            <div>
-                <form onSubmit={this.submitNewUser}>
-                <p>(* indicates a required field)</p>
+          <Form.Group>
+            <Form.Label>Country: *</Form.Label>
+            <CountryDropdown
+              value={this.state.countryId}
+              onChange={this.onChange}
+              required={true}
+            />
+          </Form.Group>
 
-                    <Form.Group>
-                        <Form.Label>
-                            First Name: *
-                        </Form.Label>
-                        <Form.Control type="text" placeholder="First Name" 
-                            name="firstName" onChange={this.onChange} 
-                            value={this.state.firstName} required/>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label>
-                            Last Name: *
-                        </Form.Label>
-                        <Form.Control type="text" placeholder="Last Name" 
-                            name="lastName" onChange={this.onChange} 
-                            value={this.state.lastName} required/>
-                    </Form.Group>
+          <Form.Group>
+            <Form.Label>Nationality: *</Form.Label>
+            <NationalityDropdown
+              value={this.state.nationality}
+              onChange={this.onChange}
+              required={true}
+            />
+          </Form.Group>
 
-                    <Form.Group>
-                        <Form.Label>
-                            Postcode: *
-                        </Form.Label>
-                        <Form.Control type="text" placeholder="Postcode" 
-                            name="postCode" onChange={this.onChange} 
-                            value={this.state.postCode} required/>
-                    </Form.Group>
+          <Form.Group>
+            <Form.Label>Date of Birth: *</Form.Label>
+            <Form.Control
+              type="date"
+              name="dateOfBirth"
+              onChange={this.onChange}
+              value={this.state.dateOfBirth}
+              required
+            />
+          </Form.Group>
 
-                    <Form.Group>
-                        <Form.Label>
-                        Country: *
-                        </Form.Label>
-                        <CountryDropdown value={this.state.countryId} onChange={this.onChange} required={true}/>
+          <Form.Group>
+            <Form.Label>Full Address: *</Form.Label>
+            <Form.Control
+              type="text"
+              name="fullAddress"
+              onChange={this.onChange}
+              value={this.state.fullAddress}
+              required
+            />
+          </Form.Group>
+          <hr />
 
-                    </Form.Group>
+          <input
+            type="submit"
+            className="btn btn-info"
+            value="Register to vote"
+          />
+        </form>
+      </div>
+    );
+  }
 
-                    <Form.Group>
-                        <Form.Label>
-                        Nationality: *
-                        </Form.Label>
-                        <NationalityDropdown value={this.state.nationality} onChange={this.onChange} required={true}/>
+  render() {
+    return (
+      <div className="">
+        <button
+          name="btnRegister"
+          type="button"
+          id="register-btn"
+          className="btn btn-success"
+          data-toggle="modal"
+          data-target="registerModal"
+          onClick={this.openModal}
+        >
+          Or Register To Vote
+        </button>
+        <Modal show={this.state.modalIsOpen} onHide={this.closeModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Register</Modal.Title>
+          </Modal.Header>
 
-                    </Form.Group>
+          <Modal.Body>{this.getRegisterForm()}</Modal.Body>
 
-                    <Form.Group>
-                        <Form.Label>
-                            Date of Birth: *
-                        </Form.Label>
-                        <Form.Control type="date" 
-                            name="dateOfBirth" onChange={this.onChange} 
-                            value={this.state.dateOfBirth} required/>
-                    </Form.Group>
-
-                    <Form.Group>
-                        <Form.Label>
-                            Full Address: *
-                        </Form.Label>
-                        <Form.Control type="text" 
-                            name="fullAddress" onChange={this.onChange} 
-                            value={this.state.fullAddress} required/>
-                    </Form.Group>
-                    <hr />
-
-                    <input type="submit" className="btn btn-info" value="Register to vote"/>
-                </form>
-            </div>
-        );
-    }
-
-    render() {
-        return (
-        <div className="">
-
-            <button type="button" id="register-btn" className="btn btn-success" data-toggle="modal" data-target="registerModal" onClick={this.openModal}>
-                Or Register To Vote 
-            </button>
-            <Modal show={this.state.modalIsOpen} onHide={this.closeModal}>
-                <Modal.Header closeButton>
-                <Modal.Title>Register</Modal.Title>
-                </Modal.Header>
-
-                <Modal.Body>
-                    {this.getRegisterForm()}
-                </Modal.Body>
-
-                <Modal.Footer>
-                </Modal.Footer>
-            </Modal>
-        </div>
-        );
-    }
+          <Modal.Footer />
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default RegisterModalButton;
