2ec08d99db9a915cedb6e8dff9e51e64e0d1f9fa
Matt <b5010228@my.shu.ac.uk>
2019-03-19 20:26:09 +0000

    removed ross' dodgy merge code

--- src/Factories/ChartFactory.jsx
+++ src/Factories/ChartFactory.jsx
@@ -1,7 +1,4 @@
-import FirstPastThePost from "../components/VotingTypes/FirstPastThePost";
 import React, { Component } from "react";
-import PreferentialVoting from "../components/VotingTypes/PreferentialVoting";
-import SingleTransferrableVote from "../components/VotingTypes/SingleTransferrableVote";
 import PieChartComponent from "../components/Charts/PieChart";
 export default class ChartFactory {
   static build(data) {
